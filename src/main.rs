84066658f79f4f5558827cbd2a9c848134ad43f0
hms5232 <hms5232@hhming.moe>
2023-12-04 20:05:16 +0800

    refactor: separate calculation logic from main

--- src/main.rs
+++ src/main.rs
@@ -12,18 +12,7 @@ fn main() {
     }
     check_input(&args).expect("Bad arguments: Non-integer found");  // make sure all input is number
 
-    println!("Total to be allocated: {}", &args[1]);
-    let total = Money::from_str(&args[1], iso::TWD).unwrap();  // total money to be allocated
-    let mut ratios: Vec<i32> = vec![];  // allocated ratio, also is subtotal of each one
-    // make ratios
-    for i in 2..args.len() {
-        ratios.push((&args[i]).parse::<i32>().unwrap());
-    }
-    let allocated = total.allocate(ratios).unwrap();  // allocated result (sorted as ratios)
-    // print result with origin price
-    for i in 2..args.len() {
-        println!("No.{} join allocated event with ${} and should pay ${}", i -1 , &args[i], allocated.get(i-2).unwrap().amount());
-    }
+    calculate(&args[1..args.len()]);
 }
 
 /// Check if all arguments is number.
@@ -38,4 +27,20 @@ fn check_input(args: &Vec<String>) -> Result<(), io::Error> {
         }
     }
     Ok(())
+}
+
+/// Calculate and print result
+fn calculate(input: &[String]) {
+    println!("Total to be allocated: {}", &input[0]);
+    let total = Money::from_str(&input[0], iso::TWD).unwrap();  // total money to be allocated
+    let mut ratios: Vec<i32> = vec![];  // allocated ratio, also is subtotal of each one
+    // make ratios
+    for i in 1..input.len() {
+        ratios.push((&input[i]).parse::<i32>().unwrap());
+    }
+    let allocated = total.allocate(ratios).unwrap();  // allocated result (sorted as ratios)
+    // print result with origin price
+    for i in 1..input.len() {
+        println!("No.{} join allocated event with ${} and should pay ${}", i, &input[i], allocated.get(i - 1).unwrap().amount());
+    }
 }
